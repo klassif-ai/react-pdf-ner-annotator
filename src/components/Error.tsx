3543a20845c607884b85c1357954ca64571c0489
Maarten <bloemen.maarten@hotmail.com>
2021-07-20 14:02:39 +0200

    Updated error visual

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import errorVisual from '../assets/icons/error.svg';
 
 interface Props {
   message?: string;
 }
 
-const Error = ({ message = 'Something went wrong' }: Props) => {
+const Error = ({ message = 'Something went wrong.' }: Props) => {
   return (
     <div className="error-container">
-      { message }
+      <img className="error-image" src={errorVisual} alt="Error" />
+      <span className="error-message">{ message }</span>
     </div>
   );
 };
